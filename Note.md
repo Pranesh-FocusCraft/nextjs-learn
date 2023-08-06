d2ee62627e2a4f6cc0ab4035c24bb415fe489639
Pranesh-FocusCraft <96162409+Pranesh-FocusCraft@users.noreply.github.com>
2023-08-06 17:13:46 +0000

    1. connected with cloud mongdb database 2. stored newsletter email id

---
+++ Note.md
@@ -0,0 +1,10 @@
+mongodb
+
+username : dbuser
+
+password : wQe2tjlJEayP8P26
+
+url : <div style=' font-size:20px; color:lightgreen '>
+mongodb+srv://dbuser:<password>@cluster0.gjftkdu.mongodb.net/
+
+</div>
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,15 +1,28 @@
-function handle(req, res) {
-	if (req.method === 'POST') {
-		const email = req.body.email
+import { MongoClient } from 'mongodb'
 
-		if (!email || !email.includes('@')) {
-			res.status(422).json({ message: 'Invalid email address.' })
-			return
-		}
+async function storeToDataBase(email) {
+  const client = await MongoClient.connect(
+    'mongodb+srv://dbuser:VupAGEY5JlgRluRk@cluster0.gjftkdu.mongodb.net/newsletter?retryWrites=true&w=majority'
+  )
 
-		res.status(201).json({ message: 'Signed up!' })
-		return
-	}
+  const db = client.db()
+  await db.collection('emails').insertOne({ email })
+  client.close()
+}
+
+async function handle(req, res) {
+  if (req.method === 'POST') {
+    const email = req.body.email
+
+    if (!email || !email.includes('@')) {
+      res.status(422).json({ message: 'Invalid email address.' })
+      return
+    }
+
+    await storeToDataBase(email)
+
+    res.status(201).json({ message: 'Signed up!' })
+  }
 }
 
 export default handle
